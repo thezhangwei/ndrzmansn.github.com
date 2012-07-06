353ce6c32722b94aaf7e583d4843ad5a76c70843
Wei Zhang <ndrzmansn@gmail.com>
2012-07-05 21:07:59 -0700

    pretty much finalized first version of weibo plugin.

--- source/javascripts/weibo.js
+++ source/javascripts/weibo.js
@@ -7,18 +7,19 @@ function showWeibos(statuses, count) {
     var weibos = document.getElementById('weibos');
     var content = '';
     statuses = statuses.slice(0, count);
+    var userid = statuses[0].user.idstr;
 
     for (var i in statuses) {
         content += '<li>' + '<p>';
         var status = statuses[i];
         var text = '';
-        text += getDateAndURL(status);
-        text += status['text'];
+        text += getDateAndURL(status, userid);
+        text += linkifyStatus(status['text']);
 
         if (status['retweeted_status']) {
             status = status['retweeted_status'];
-            text += '//@' + status['user']['name'] + ':';
-            text += status['text'];
+            text += '//' + linkifyUser(status['user']) + ':';
+            text += linkifyStatus(status['text']);
         }
 
         content += text;
@@ -28,109 +29,29 @@ function showWeibos(statuses, count) {
     weibos.innerHTML = content;
 }
 
-function getDateAndURL(status) {
-    return '<a href="http://weibo.com/2116636225/' + WeiboUtil.mid2url(status['mid']) + '">'
+function getDateAndURL(status, userid) {
+    return '<a href="http://weibo.com/' + userid + '/' + WeiboUtil.mid2url(status['mid']) + '">'
              + prettyDate(status.created_at) + '</a>';
 }
 
-// copied from https://gist.github.com/1844413 by fengmk2
-/**
- * 新浪微博mid与url互转实用工具
- * 作者: XiNGRZ (http://weibo.com/xingrz)
- */
+// pretty much copied from twitter.js
+function linkifyStatus(text) {
+    // Chomp http:// off the text of the link
+    text = text.replace(/(https?:\/\/)([\w\-:;?&=+.%#\/]+)/gi, '<a href="$1$2">$2</a>');
+    // cannot get user url using screen name, need userid. so redirect to search the screen name
+    text = text.replace(/(^|\W)@(\S+)\:/g, '$1<a href="http://s.weibo.com/weibo/$2">@$2</a>:');
+    // linkify topic, not tested yet
+    text = text.replace(/(^|\W)#(\S+)/g, '$1<a href="http://s.weibo.com/weibo/$2">#$2</a>');
 
-var WeiboUtil = {
-    // 62进制字典
-    str62keys: [
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    ],
-};
+    return text
+}
 
-/**
- * 62进制值转换为10进制
- * @param {String} str62 62进制值
- * @return {String} 10进制值
- */
-WeiboUtil.str62to10 = function(str62) {
-	var i10 = 0;
-	for (var i = 0; i < str62.length; i++)
-	{
-		var n = str62.length - i - 1;
-		var s = str62[i];
-		i10 += this.str62keys.indexOf(s) * Math.pow(62, n);
-	}
-	return i10;
-};
-
-/**
- * 10进制值转换为62进制
- * @param {String} int10 10进制值
- * @return {String} 62进制值
- */
-WeiboUtil.int10to62 = function(int10) {
-	var s62 = '';
-	var r = 0;
-	while (int10 != 0 && s62.length < 100) {
-		r = int10 % 62;
-		s62 = this.str62keys[r] + s62;
-		int10 = Math.floor(int10 / 62);
-	}
-	return s62;
-};
-
-/**
- * URL字符转换为mid
- * @param {String} url 微博URL字符，如 "wr4mOFqpbO"
- * @return {String} 微博mid，如 "201110410216293360"
- */
-WeiboUtil.url2mid = function(url) {
-	var mid = '';
-	
-	for (var i = url.length - 4; i > -4; i = i - 4)	//从最后往前以4字节为一组读取URL字符
-	{
-		var offset1 = i < 0 ? 0 : i;
-		var offset2 = i + 4;
-		var str = url.substring(offset1, offset2);
-		
-		str = this.str62to10(str);
-		if (offset1 > 0)	//若不是第一组，则不足7位补0
-		{
-			while (str.length < 7)
-			{
-				str = '0' + str;
-			}
-		}
-		
-		mid = str + mid;
-	}
-	
-	return mid;
-};
-
-/**
- * mid转换为URL字符
- * @param {String} mid 微博mid，如 "201110410216293360"
- * @return {String} 微博URL字符，如 "wr4mOFqpbO"
- */
-WeiboUtil.mid2url = function(mid) {
-    if(!mid) {
-        return mid;
+function linkifyUser(user) {
+    if (!user) {
+        return '';
     }
-    mid = String(mid); //mid数值较大，必须为字符串！
-	if(!/^\d+$/.test(mid)){ return mid; }
-	var url = '';
-	
-	for (var i = mid.length - 7; i > -7; i = i - 7)	//从最后往前以7字节为一组读取mid
-	{
-		var offset1 = i < 0 ? 0 : i;
-		var offset2 = i + 7;
-		var num = mid.substring(offset1, offset2);
-		
-		num = this.int10to62(num);
-		url = num + url;
-	}
-	
-	return url;
-};
+
+    var profile_url = user.profile_url;
+    var name = user.name;
+    return '<a href="http://weibo.com/' + profile_url + '">@' + name + '</a>';
+}
